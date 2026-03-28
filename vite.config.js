cea24a891429f4dae02b168bdbf8f85315ab038b
mdazam26 <azam456alam@gmail.com>
2026-03-28 07:50:39 +0000

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
 // // https://vite.dev/config/
 
@@ -51,9 +51,9 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//   },
-// });
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+});
